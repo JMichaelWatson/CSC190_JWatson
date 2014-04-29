4b52a42cd1f6a287801971d3f2723fade91a6f59
Michael W. <jwatson@student.neumont.edu>
2014-04-28 21:05:43 -0600

    Stopping point save
    Saving and stoping  because of questions.

--- GameSkeleton/GameSolution/Engine/Vector2D.h
+++ GameSkeleton/GameSolution/Engine/Vector2D.h
@@ -16,6 +16,52 @@ namespace Engine
 	Vector2D operator+(const Vector2D& left , const Vector2D& right){
 		return Vector2D(left.x + right.x, left.y + right.y);
 	}
+
+	Vector2D operator-(const Vector2D& left,  const Vector2D& right){
+		return Vector2D(left.x - right.x, left.y - right.y);
+	}
+
+	Vector2D operator*(const Vector2D& vector, const float& scaler){
+		return Vector2D(vector.x * scaler, vector.y * scaler);
+	}
+
+	Vector2D operator*(const float& scaler, const Vector2D& vector){
+		return Vector2D(vector.x * scaler, vector.y * scaler);
+	}
+
+	Vector2D operator/(const Vector2D& vector, const float& scaler){
+		return Vector2D(vector.x / scaler, vector.y / scaler);
+	}
+
+	Vector2D LERP(const Vector2D& left, const Vector2D& right, const float& beta){
+		return Vector2D(((1-beta)*left.x + (beta*right.x)),  ((1-beta)*left.y + (beta*right.y)));
+	}
+
+	float Length(const Vector2D& vector){
+		return sqrt(pow(vector.x,2) + pow(vector.y,2));
+	}
+
+	float SquareLenght(const Vector2D& vector){
+		return (pow(vector.x, 2) + pow(vector.y, 2));
+	}
+
+	Vector2D Normalized(const Vector2D& vector){
+		float length = Length(vector);
+		return vector / length;
+		
+	}
+	
+	float Dot(const Vector2D& left, const Vector2D& right){
+		return ((left.x * right.x) + (left.y * right.y));
+	}
+
+	Vector2D CW(const Vector2D& vector){
+		return Vector2D(-(vector.y), vector.x);
+	}
+
+	Vector2D CCW(const Vector2D& vector){
+		return Vector2D(vector.y, -(vector.x));
+	}
 }
 
 #endif
--- GameSkeleton/TwoDCalculator/EntryPoint.cpp
+++ GameSkeleton/TwoDCalculator/EntryPoint.cpp
@@ -9,9 +9,8 @@ Vector2D right;
 Vector2D result;
 
 void MyBasicVectorEquationCallBack(const BasicVectorEquationInfo& data){
-	left = data.scalar1 = Vector2D(data.x1, data,y1);
-	right = data.scalar2 = Vector2D(data.x2, data.y2);
-
+	left = data.scalar1 * Vector2D(data.x1, data.y1);
+	right = data.scalar2 * Vector2D(data.x2, data.y2);
 }
 
 
